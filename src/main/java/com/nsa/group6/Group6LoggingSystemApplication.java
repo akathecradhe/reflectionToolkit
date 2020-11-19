ab4a727608ee8c4a7e6879c8f51e3e9d30951dbb
Juan Creasey <creaseyja@cardiff.ac.uk>
2020-11-19 15:00:52 +0000

    Initial Commit

---
+++ src/main/java/com/nsa/group6/Group6LoggingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nsa.group6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group6LoggingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group6LoggingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nsa/group6/Group6LoggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsa.group6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group6LoggingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
