cde21cf1c10f99947bcc7b4aacff8637ea8cada2
c1919822 <rowbothamt@cardiff.ac.uk>
2020-11-22 16:31:23 +0000

    Small updates to the event domain, added user domain and basic JPA for events.

--- src/main/java/com/nsa/group6/domain/User.java
+++ src/main/java/com/nsa/group6/domain/User.java
@@ -1,4 +1,22 @@
 package com.nsa.group6.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    private String username;
+    private String name;
+    private Date dateJoined;
+    private String password;
 }
