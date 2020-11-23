4ec39feed5258f7fb95f505f5aba65f95c912364
clive <tsunguc@cardiff.ac.uk>
2020-11-23 10:35:17 +0000

    added the uKPSFdimensions TABLE in the schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,23 +7,32 @@ SET IGNORECASE=TRUE;
 -- Let's create a new table now
 
 CREATE TABLE IF NOT EXISTS `event` (
-  `eventId` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `eventId` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `event` VARCHAR(45) NOT NULL,
   `description` VARCHAR(45) NOT NULL,
   `role` VARCHAR(45) NOT NULL,
   `tag` VARCHAR(45) NOT NULL,
-  `username` VARCHAR(45) NOT NULL,
-
-  `lastupdated` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`eventId`))
+  `username` CHAR(45) NOT NULL,
+  `lastupdated` TIMESTAMP NOT NULL
+  )
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `user` (
-
-  `username` varchar(45) UNSIGNED NOT NULL ,
-  `name` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` CHAR(45) UNIQUE NOT NULL PRIMARY KEY,
+  `firstName` VARCHAR(45) NOT NULL,
   `dateJoined` DATE  NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`username`),
-  FOREIGN KEY (username) REFERENCES event(username))
+  `password` VARCHAR(45) NOT NULL
+  )
 ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `uKPSFdimensions` (
+  `dimensionID` CHAR(45) UNIQUE NOT NULL PRIMARY KEY,
+  `dimensionType` VARCHAR(45) NOT NULL,
+  `dimensionNumber` INT  NOT NULL,
+  `dimensionName` VARCHAR(45) NOT NULL,
+  `dimensionDescription` VARCHAR(45) NOT NULL
+  )
+ENGINE = InnoDB;
+
+ALTER TABLE `event`
+ADD FOREIGN KEY  (`username`) REFERENCES users(`username`);
