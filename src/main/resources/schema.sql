2ffad88cb10f5bc599a2c690b67dc6cab44850a2
clive <tsunguc@cardiff.ac.uk>
2020-11-23 13:00:00 +0000

    added the tag data into uKPSFdimensions table using the data.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,13 +12,14 @@ CREATE TABLE IF NOT EXISTS `event` (
   `description` VARCHAR(45) NOT NULL,
   `role` VARCHAR(45) NOT NULL,
   `tag` VARCHAR(45) NOT NULL,
-  `username` CHAR(45) NOT NULL,
+  `username` CHAR(45) NOT NULL ,
+  `dimensionID` int UNIQUE ,
   `lastupdated` TIMESTAMP NOT NULL
   )
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `username` CHAR(45) UNIQUE NOT NULL PRIMARY KEY,
+  `username` CHAR(45) UNIQUE NOT NULL PRIMARY KEY ,
   `firstName` VARCHAR(45) NOT NULL,
   `dateJoined` DATE  NOT NULL,
   `password` VARCHAR(45) NOT NULL
@@ -26,13 +27,16 @@ CREATE TABLE IF NOT EXISTS `users` (
 ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `uKPSFdimensions` (
-  `dimensionID` CHAR(45) UNIQUE NOT NULL PRIMARY KEY,
+  `dimensionID` int UNIQUE NOT NULL PRIMARY KEY auto_increment,
   `dimensionType` VARCHAR(45) NOT NULL,
-  `dimensionNumber` INT  NOT NULL,
-  `dimensionName` VARCHAR(45) NOT NULL,
-  `dimensionDescription` VARCHAR(45) NOT NULL
+  `dimensionKey` VARCHAR(45)  NOT NULL,
+  `dimensionName` VARCHAR(1000) NOT NULL,
+  `dimensionDescription` VARCHAR(3000) NOT NULL
   )
 ENGINE = InnoDB;
+
+ALTER TABLE `event`
+ADD FOREIGN KEY  (`dimensionID`) REFERENCES uKPSFdimensions(`dimensionID`);
 
 ALTER TABLE `event`
 ADD FOREIGN KEY  (`username`) REFERENCES users(`username`);
