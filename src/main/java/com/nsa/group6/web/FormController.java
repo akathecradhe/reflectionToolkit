1f9464284d5ff10526067a4973f95165d7195adb
c1919822 <rowbothamt@cardiff.ac.uk>
2020-11-24 16:13:29 +0000

    Added basic thymeleaf to the reflection list page.

--- src/main/java/com/nsa/group6/web/FormController.java
+++ src/main/java/com/nsa/group6/web/FormController.java
@@ -1,7 +1,19 @@
 package com.nsa.group6.web;
+import com.nsa.group6.domain.Event;
+import com.nsa.group6.domain.Form;
+import com.nsa.group6.domain.TestForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class FormController {
@@ -13,4 +25,21 @@ public class FormController {
 
         return "form";
     }
+
+    @GetMapping("reflections/{username}")
+    public String allGreetings(@PathVariable(name = "username", required = false) Optional<String> username, Model model) {
+
+//        if (username.isPresent()) {
+            //reflections = formRetriever.findReflectionsByName(username)
+//        } else {
+            //reflections = formRetriever.findReflectionsByName(currentUser.username)
+//        }
+        TestForm form1 = new TestForm("Design Or Plan", "DESCRIPTION HERE REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE","21/11/2020","22/11/2020 11:34");
+        System.out.println(form1.getEventDate());
+        model.addAttribute("form1", form1);
+
+        return "reflection-list";
+
+    }
+
 }
