9bfdc12b19446a46660810f3d4e3761ef5249c6c
c1919822 <rowbothamt@cardiff.ac.uk>
2020-11-28 13:34:19 +0000

    Added constructors to tags to differentiate UKPSF with errors for incorrect usage. Wrote JUnit Tests to correlate

---
+++ src/test/java/com/nsa/group6/DomainTests.java
@@ -0,0 +1,30 @@
+package com.nsa.group6;
+
+import com.nsa.group6.domain.Tags;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+
+public class DomainTests {
+
+    @Test
+    public void UKPSF_IncorrectConstructor() throws Exception {
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            new Tags(123,"UKPSF","Teaching Name");
+        });
+    }
+
+    @Test
+    public void Tag_IncorrectConstructor() throws Exception {
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            new Tags(123,"Teaching","Teaching Name","A1","Description...");
+        });
+    }
+
+    @Test
+    public void Tag_And_UKPSF_LegalConstructor() throws Exception {
+        Tags ukpsf = new Tags(123,"UKPSF","Teaching Name","A1","Description...");
+        Tags tag = new Tags(123,"teaching","Teaching Name");
+    }
+}
