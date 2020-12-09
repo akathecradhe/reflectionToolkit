a0878c85a5912e2c1742e284d63f5bcae8ef774c
clive <tsunguc@cardiff.ac.uk>
2020-12-09 18:25:27 +0000

    created mapping for edit tag page and added functionaility to add new thought cloud

--- src/main/java/com/nsa/group6/web/AdminController.java
+++ src/main/java/com/nsa/group6/web/AdminController.java
@@ -1,10 +1,41 @@
 package com.nsa.group6.web;
 
+import com.nsa.group6.domain.Tags;
+import com.nsa.group6.service.TagService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
+@Controller
 public class AdminController {
 
+    private final TagService tagService;
+
+    public AdminController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    // admin
+    @GetMapping("/admin/edit-tags")
+
+    public String getAllThoughtClouds(Model model) {
+
+        List<Tags> allThoughtClouds = tagService.getTagByCategory("Thought Cloud");
+        System.out.println(allThoughtClouds);
+        model.addAttribute("ThoughtTags",allThoughtClouds);
+
+        return "admintags";
+    }
+
+    @PostMapping("/addNew")
+    public String addThoughtCloud(Tags tag){
+
+        return  "redirect:/admintags";
+    }
 
 
 
