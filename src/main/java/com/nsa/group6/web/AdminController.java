60e6535dca052448851c2bd5682641884a2e41ff
clive <tsunguc@cardiff.ac.uk>
2020-12-09 22:40:34 +0000

    add new tag method implemented

--- src/main/java/com/nsa/group6/web/AdminController.java
+++ src/main/java/com/nsa/group6/web/AdminController.java
@@ -1,11 +1,14 @@
 package com.nsa.group6.web;
 
 import com.nsa.group6.domain.Tags;
+import com.nsa.group6.repositories.TagsRepository;
 import com.nsa.group6.service.TagService;
+import com.nsa.group6.service.TagsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,10 +16,12 @@ import java.util.List;
 @Controller
 public class AdminController {
 
-    private final TagService tagService;
 
-    public AdminController(TagService tagService) {
-        this.tagService = tagService;
+    private final TagsService tagsService;
+
+    public AdminController(TagService tagService, TagsService tagsService, TagsRepository tagsRepository) {
+        this.tagsService = tagsService;
+
     }
 
     // admin
@@ -24,17 +29,32 @@ public class AdminController {
 
     public String getAllThoughtClouds(Model model) {
 
-        List<Tags> allThoughtClouds = tagService.getTagByCategory("Thought Cloud");
+        List<Tags> allThoughtClouds = tagsService.getTagByCategory("Thought Cloud");
         System.out.println(allThoughtClouds);
         model.addAttribute("ThoughtTags",allThoughtClouds);
 
         return "admintags";
     }
 
-    @PostMapping("/addNew")
-    public String addThoughtCloud(Tags tag){
+    @RequestMapping("/addNew")
+    public String addThoughtCloud(Model model,
+                                  @RequestParam String
+                                          tagNameAdd) throws Exception {
 
-        return  "redirect:/admintags";
+        System.out.println(tagNameAdd);
+
+        List<Tags> allTags = tagsService.getAllTags();
+
+        int latestTagId = allTags.size()+1;
+
+        Tags newThought  = new Tags(latestTagId,"Thought Cloud",tagNameAdd);
+
+       tagsService.saveTags(newThought);
+
+        getAllThoughtClouds(model);
+
+
+         return "admintags";
     }
 
 
